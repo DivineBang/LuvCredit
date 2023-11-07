397ee48cb1d56cb20e3394c98da0573e1ff58e44
Dmytro Didenko <dmitriydidenko02@gmail.com>
2023-11-07 16:29:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+//preloader
+$(document).ready(function() {
+    $('#preloader').fadeOut(400);
+  });  
+
+  $(document).ready(function(){
+    $('.center').slick({
+      centerMode: true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      speed: 1500,
+      centerPadding: '10px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 993,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '10px',
+            slidesToShow: 2
+          }
+        },
+        
+        {
+          breakpoint: 769,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '10px',
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 577,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '5px',
+            slidesToShow: 1
+          }
+          
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '0',
+            slidesToShow: 1
+          }
+          
+        }
+        
+      ]
+    });
+  });
+
+/*   Tab */
+  document.querySelectorAll('.card__wrapp').forEach(card => {
+    const text = card.querySelector('.card__text');
+    const img = card.querySelector('.card__wrapp_img');
+    card.addEventListener('click', () => {
+        card.classList.toggle('active');
+        if (card.classList.contains('active')) {
+            text.style.maxHeight = text.scrollHeight + 'px';
+            img.src = 'assets/img/minus.png';
+        } else {
+            text.style.maxHeight = '0';
+            img.src = 'assets/img/plus.png';
+            // Ожидание завершения анимации
+            text.addEventListener('transitionend', () => {
+                if (!card.classList.contains('active')) {
+                    text.style.display = 'none';
+                }
+            });
+        }
+    });
+});
+
+
+//Прокрут до секции 
+document.addEventListener('DOMContentLoaded', function() {
+  const menuLinks = document.querySelectorAll('.lender__btn');
+
+  menuLinks.forEach(link => {
+      link.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          const targetId = link.getAttribute('href'); // Получаем атрибут href ссылки
+          const targetSection = document.querySelector(targetId); // Находим соответствующую секцию по ID
+
+          if (targetSection) {
+              targetSection.scrollIntoView({ behavior: 'smooth' }); // Прокручиваем к секции плавно
+          }
+      });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+  
+  
